bbbbfea099b91fb77ae8dfc2f25e1eccbc345ca0
Golbakh Mikhail <golbah@yandex.ru>
2020-12-05 09:04:06 +0300

    test: make tests for find_min_brent

---
+++ tests/test_find_min_brent.py
@@ -0,0 +1,29 @@
+import unittest
+from tests.defaults_functions import epsilon, places, test_functions
+from algo.find_min_brent import find_min_brent
+
+
+class TestFindMinBrent(unittest.TestCase):
+    def test_func1(self):
+        func, a, b, result = test_functions[0]
+        self.assertAlmostEqual(find_min_brent(func, a, b, epsilon=epsilon), result, places=places)
+
+    def test_func2(self):
+        func, a, b, result = test_functions[1]
+        self.assertAlmostEqual(find_min_brent(func, a, b, epsilon=epsilon), result, places=places)
+
+    def test_func3(self):
+        func, a, b, result = test_functions[2]
+        self.assertAlmostEqual(find_min_brent(func, a, b, epsilon=epsilon), result, places=places)
+
+    def test_func4(self):
+        func, a, b, result = test_functions[3]
+        self.assertAlmostEqual(find_min_brent(func, a, b, epsilon=epsilon), result, places=places)
+
+    def test_func5(self):
+        func, a, b, result = test_functions[4]
+        self.assertAlmostEqual(find_min_brent(func, a, b, epsilon=epsilon), result, places=places)
+
+
+if __name__ == '__main__':
+    unittest.main()
